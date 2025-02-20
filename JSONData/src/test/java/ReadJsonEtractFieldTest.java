87fd121123ddcecb302f75591d5c89cda0f27c2b
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-02-20 15:40:35 +0530

    [Naincy] add Extract required fields

---
+++ JSONData/src/test/java/ReadJsonEtractFieldTest.java
@@ -0,0 +1,20 @@
+package com.practiceproblems.json;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.*;
+
+public class ReadJsonExtractFields {
+    public static void main(String[] args) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(new File("src/main/resources/output.json"));
+            String brand = rootNode.get("brand").asText();
+            String model = rootNode.get("model").asText();
+
+            System.out.println("Name: " + brand);
+            System.out.println("Email: " + model);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
